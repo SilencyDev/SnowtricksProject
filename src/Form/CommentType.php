67bf56a0516cad2241506e71105313d899080493
SilencyDev <kvn.macquet@gmail.com>
2020-03-05 02:43:42 +0100

    added galery with click to show  functionnality for images

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,23 +3,19 @@
 namespace App\Form;
 
 use App\Entity\Comment;
-use App\Entity\Snowtrick;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Security;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', TextType::class)
+            ->add('content', TextType::class ,[
+                'label' => false,
+            ])
             ;
     }
 
